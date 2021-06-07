e5de42882510f1a64a95b7c9831f5997b23079f7
romeo adjam <sodangbe@users.noreply.github.com>
2021-06-07 07:24:29 -0500

    Add project files.

---
+++ CalculatorHBK/Calculator.cs
@@ -0,0 +1,192 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CalculatorHBK
+{
+    public class Calculator
+    {
+        //Encapsulation of the calculator properties 
+
+        //Allowed operators as a string of 4 characters 
+        private readonly string AllowedOperator = "+-*/";
+
+        // Declaring left property with Get and Set methods
+
+        private string _left;   //backing field
+        //getter
+        public string GetLeft()
+        {
+            return _left;
+
+        }
+
+        //setter
+        public void SetLeft(string value)
+        {
+            if (isNumeric(value) && !string.IsNullOrEmpty(value))
+            {
+                _left = value; ;
+            }
+            else
+            {
+                throw new ArgumentException($"Invalid Number, {value}");
+
+            }
+
+        }
+
+        // Declaring right property with Get and Set methods
+
+        private string _right;  //backing field
+
+        //public string Right { get => _right; set => _right = value; } auto-implemented properties will not allow for validation 
+
+        //getter
+        public string GetRight()
+        {
+            return _right;
+
+        }
+
+        //setter
+        public void SetRight(string value)
+        {
+            if (isNumeric(value) && !string.IsNullOrEmpty(value))
+            {
+                _right = value; ;
+            }
+            else
+            {
+                throw new ArgumentException($"Invalid Number, {value}");
+
+            }
+
+        }
+
+        // Declaring operator property with Get and Set methods
+
+        private string _operator; //backing field
+
+        //getter
+        public string GetOperator()
+        {
+            return _operator;
+
+        }
+
+        //setter
+        public void SetOperator(string value)
+        {
+
+
+            if (this.AllowedOperator.Contains(value))
+            {
+                _operator = value;
+            }
+            else
+            {
+                throw new ArgumentException($"Operator Not Allowed,{value}");
+
+            }
+
+        }
+
+        //class calculator default constructor
+        public Calculator() { }
+
+        //class calculator constructor overloading 
+        public Calculator(string left, string right, string allowedOperator)
+        {
+                       
+            if(isNumeric(left) && !string.IsNullOrEmpty(left))
+            {
+                _left = left;
+            }
+            else
+            {
+               throw new ArgumentException($"Invalid Number, {left}");
+
+            }
+            
+            if (isNumeric(right) && !string.IsNullOrEmpty(left))
+            {
+                _right = right;
+            }
+            else
+            {
+                throw new ArgumentException($"Invalid Number, {right}");
+
+            }
+            if (AllowedOperator.Contains(allowedOperator))
+            {
+                _operator = allowedOperator;
+            }
+            else
+            {
+                throw new ArgumentException($"Operator Not Allowed,{allowedOperator}");
+
+            }
+        }
+
+        // Method Calculate 
+        public int Calculate()
+        {
+            //int.Parse(this.left) != Convert.ToInt32(this._left);  //int.parse the other casting alternative will throw an ArgumentNullException when passing a null value
+
+            int result = 0;
+
+
+            //addition 
+            if (this._operator == "+")
+                //checking for aritmetic flow
+                checked
+                {
+                    result += Convert.ToInt32(this._left) + Convert.ToInt32(this._right);
+                }
+            //multiplication
+            if (this._operator == "*")
+                //checking for arithmetic flow 
+                checked
+                {
+                    result += Convert.ToInt32(this._left) * Convert.ToInt32(this._right);
+                }
+            //substraction
+            if (this._operator == "-")
+                //checking for arithmetic flow 
+                checked
+                {
+                    result += Convert.ToInt32(this._left) - Convert.ToInt32(this._right);
+                }
+            //division 
+            if ((this._operator == "/"))
+            checked
+            {
+                result += Convert.ToInt32(this._left) / Convert.ToInt32(this._right);
+            }
+
+            return result;
+        }
+        public string GetResult()
+        {
+            return Calculate().ToString();
+
+        }
+
+#pragma warning disable IDE0060 // Remove unused parameter
+        public int GetPreviousResult(int index)
+        {
+
+            return index;
+
+        }
+#pragma warning restore IDE0060 // Remove unused parameter
+        //Method to check if the property is numeric
+        public static bool isNumeric(string s)
+        {
+            return double.TryParse(s, out double n);
+        }
+    }
+}
---
+++ Calcultor.Test/CalculatorTest.cs
@@ -0,0 +1,142 @@
+﻿using CalculatorHBK;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace Calcultor.Test
+{
+    [TestClass]
+    public class CalculatorTest
+    {
+        [TestMethod]
+        public void Test_Calculate_Addition()
+        {
+            //Arrange 
+            int expected = 10;
+
+            Calculator myCalc = new Calculator("9", "1", "+");
+
+            //Act
+            int actual = myCalc.Calculate();
+
+            //Assert 
+            Assert.AreEqual(expected, actual);
+
+        }
+        [TestMethod]
+        public void Test_Calculate_Substraction()
+        {
+            //Arrange 
+            int expected = 11;
+            Calculator myCalc = new Calculator("4", "-7", "-");
+
+            //Act
+            int actual = myCalc.Calculate();
+
+            //Assert 
+            Assert.AreEqual(expected, actual);
+
+        }
+
+        [TestMethod]
+        public void Test_Calculate_Mulltiplication()
+        {
+            //Arrange 
+            int expected = 64;
+
+            Calculator myCalc = new Calculator("8", "8", "*");
+
+            //Act
+            int actual = myCalc.Calculate();
+
+            //Assert 
+            Assert.AreEqual(expected, actual);
+
+        }
+
+        [TestMethod]
+        public void Test_Calculate_Division()
+        {
+            //Arrange 
+            int expected = 4;
+
+            Calculator myCalc = new Calculator("12", "3", "/");
+
+            //Act
+            int actual = myCalc.Calculate();
+
+            //Assert 
+            Assert.AreEqual(expected, actual);
+
+        }
+
+
+        [TestMethod]
+        [ExpectedException(typeof(DivideByZeroException))]
+        public void Test_Calculate_DivisionbyZero()
+        {
+            //Arrange 
+            Calculator myCalc = new Calculator("256", "0", "/");
+            int actual = myCalc.Calculate();
+
+            //Assert 
+            Assert.AreEqual(256, actual);
+
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentException))]
+        public void Test_Calculate_AdditionWithEmptyString()
+        {
+            //Arrange 
+            Calculator myCalc = new Calculator();
+            myCalc.SetLeft(" ");
+            myCalc.SetRight("423");
+            myCalc.SetOperator("+");
+
+            //Act
+            int actual = myCalc.Calculate();
+
+            //Assert 
+            Assert.AreEqual(423, actual);
+
+
+        }
+
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentException))]
+        public void Test_Calculate_AdditionWithString()
+        {
+            //Arrange 
+            Calculator myCalc = new Calculator();
+            myCalc.SetLeft("foo");
+            myCalc.SetRight("14");
+            myCalc.SetOperator("+");
+
+            //Act
+            int actual = myCalc.Calculate();
+
+            //Assert 
+            Assert.AreEqual(14, actual);
+
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(OverflowException))]
+        public void Test_Calculate_ArithmeticOverflow()
+        {
+            //Arrange 
+            Calculator myCalc = new Calculator();
+            myCalc.SetLeft("2147483647");
+            myCalc.SetRight("1");
+            myCalc.SetOperator("+");
+
+            //Act
+            int actual = myCalc.Calculate();
+
+            //Assert 
+            Assert.AreEqual(-2147483648, actual);
+
+        }
+    }
+}
